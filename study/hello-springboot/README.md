63c07eaaf32e4f5dfbe437c35fa815577e705864
EunjinJwa <ejjwa0563@gmail.com>
2021-06-20 00:14:23 +0900

    inflean 강의 - hello-spring

---
+++ study/hello-springboot/README.md
@@ -0,0 +1,9 @@
+Inflearn 강의 : Spring 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
+
+* project spec
+    * springboot 2.4.7
+    * gradle
+    * java 1.8 
+    
+
+ 
---
+++ study/hello-springboot/src/main/java/dev/jinny/java/study/hellospringboot/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package dev.jinny.java.study.hellospringboot.repository;
+
+import dev.jinny.java.study.hellospringboot.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
