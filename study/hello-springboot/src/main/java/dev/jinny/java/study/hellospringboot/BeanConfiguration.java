4991ff426ff77c77cda5af2e164dae21e21e4d2a
EunjinJwa <ejjwa0563@gmail.com>
2021-06-20 22:54:22 +0900

    inflean 강의 - hello-spring 완료

---
+++ study/hello-springboot/src/main/java/dev/jinny/java/study/hellospringboot/BeanConfiguration.java
@@ -0,0 +1,74 @@
+package dev.jinny.java.study.hellospringboot;
+
+import dev.jinny.java.study.hellospringboot.aop.TimeTraceAop;
+import dev.jinny.java.study.hellospringboot.repository.*;
+import dev.jinny.java.study.hellospringboot.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+@Configuration
+public class BeanConfiguration {
+
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public BeanConfiguration(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+
+
+//    @PersistenceContext
+//    private EntityManager em;
+//
+
+
+    /**
+     * Spring Data JPA
+     */
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public BeanConfiguration(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /**
+     * 빈 직접 생성 방법
+     * @Bean 어노테이션으로 생성된 메서드를 Spring 컨테이너에 Bean으로 등록해준.
+      */
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//
+//        /**
+//         * 인터페이스에서 구현체 변경을 쉽게 할 수 있다.
+//         */
+//
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//
+//    }
+
+
+
+
+
+
+
+
+}
