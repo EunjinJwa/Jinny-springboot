b231977d74516a48e66d4319fb0b1848b3148d63
EunjinJwa <ejjwa0563@gmail.com>
2021-08-25 14:49:53 +0900

    외장tomcat 설정

--- springboot/springboot-sample/src/main/java/springboot/sample/WorkerApplication.java
+++ springboot/springboot-sample/src/main/java/springboot/sample/WorkerApplication.java
@@ -2,12 +2,24 @@ package springboot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class WorkerApplication {
+public class WorkerApplication extends SpringBootServletInitializer {
+
+    /**
+     * springboot 외장 톰캣 사용시 SpringBootServletInitializer 를 extend 받아서 사용해야 함
+     * @param application
+     * @return
+     */
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WorkerApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(WorkerApplication.class);
--- springboot/springboot-sample/src/main/java/springboot/sample/configuration/ShutdownApplication.java
+++ springboot/springboot-sample/src/main/java/springboot/sample/configuration/ShutdownApplication.java
@@ -1,56 +1,56 @@
-package springboot.sample.configuration;
-import org.apache.catalina.connector.Connector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextClosedEvent;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-public class ShutdownApplication implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
-
-    private static final Logger log = LoggerFactory.getLogger(ShutdownApplication.class);
-
-    private static final int TIMEOUT = 30;
-
-    private volatile Connector connector;
-
-    @Override
-    public void customize(Connector connector) {
-        this.connector = connector;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextClosedEvent event) {
-        this.connector.pause();
-        Executor executor = this.connector.getProtocolHandler().getExecutor();
-        if (executor instanceof ThreadPoolExecutor) {
-            try {
-                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                log.info("* * * Application Shutdown. * * * ");
-                log.info("threadPoolExecutor.getActiveCount() : {}", threadPoolExecutor.getActiveCount());
-                log.info("threadPoolExecutor.getTaskCount() : {}",threadPoolExecutor.getTaskCount());
-
-                /**
-                 * 진행중인 스레드는 대기를 타고 종료를 시키나, 스케쥴러는 적용 안됨.
-                 */
-                threadPoolExecutor.shutdown();
-                if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
-                    log.warn("* * * Tomcat thread pool did not shut down gracefully within "
-                            + TIMEOUT + " seconds. Proceeding with forceful shutdown");
-
-                    threadPoolExecutor.shutdownNow();
-
-                    if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
-                        log.error("* * * Tomcat thread pool did not terminate");
-                    }
-                }
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-}
+//package springboot.sample.configuration;
+//import org.apache.catalina.connector.Connector;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextClosedEvent;
+//
+//import java.util.concurrent.Executor;
+//import java.util.concurrent.ThreadPoolExecutor;
+//import java.util.concurrent.TimeUnit;
+//
+//public class ShutdownApplication implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+//
+//    private static final Logger log = LoggerFactory.getLogger(ShutdownApplication.class);
+//
+//    private static final int TIMEOUT = 30;
+//
+//    private volatile Connector connector;
+//
+//    @Override
+//    public void customize(Connector connector) {
+//        this.connector = connector;
+//    }
+//
+//    @Override
+//    public void onApplicationEvent(ContextClosedEvent event) {
+//        this.connector.pause();
+//        Executor executor = this.connector.getProtocolHandler().getExecutor();
+//        if (executor instanceof ThreadPoolExecutor) {
+//            try {
+//                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+//                log.info("* * * Application Shutdown. * * * ");
+//                log.info("threadPoolExecutor.getActiveCount() : {}", threadPoolExecutor.getActiveCount());
+//                log.info("threadPoolExecutor.getTaskCount() : {}",threadPoolExecutor.getTaskCount());
+//
+//                /**
+//                 * 진행중인 스레드는 대기를 타고 종료를 시키나, 스케쥴러는 적용 안됨.
+//                 */
+//                threadPoolExecutor.shutdown();
+//                if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+//                    log.warn("* * * Tomcat thread pool did not shut down gracefully within "
+//                            + TIMEOUT + " seconds. Proceeding with forceful shutdown");
+//
+//                    threadPoolExecutor.shutdownNow();
+//
+//                    if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+//                        log.error("* * * Tomcat thread pool did not terminate");
+//                    }
+//                }
+//            } catch (InterruptedException ex) {
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+//    }
+//}
