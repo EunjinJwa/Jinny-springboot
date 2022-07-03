0ce3a06ef6e57773ffb263d2705932e017309855
EunjinJwa <ejjwa0563@gmail.com>
2022-07-03 17:14:16 +0900

    springboot-toy 생성

---
+++ springboot-toy/src/main/java/jinny/springboot/toy/SpringbootToyApplication.java
@@ -0,0 +1,13 @@
+package jinny.springboot.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootToyApplication.class, args);
+	}
+
+}
---
+++ springboot-toy/src/test/java/jinny/springboot/toy/SpringbootToyApplicationTests.java
@@ -0,0 +1,13 @@
+package jinny.springboot.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
